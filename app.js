5805921c35df3ae940d79baf404c8dd28de15317
anfego1 <anfego1@hotmail.com>
2024-10-28 18:36:11 +0000

    verificar 14

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ function executePm2Restart(gitOutput) {
 // Ruta GET para verificar la webhook de github
 app.get('/verificar', async (req, res) => {
   try {
-    const response = [true, `verificar 13`, 200];
+    const response = [true, `verificar 14`, 200];
     res.status(200).json(response);
   } catch (error) {
     const response = [false, error, 500];
